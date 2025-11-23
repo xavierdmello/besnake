e7143a5479e2d7cb37f9e2b9a380ed6d0cbeacb7
Xavier D'Mello <xaviermdmello@gmail.com>
2025-11-23 04:38:06 -0300

    update abi

--- frontend/abi.ts
+++ frontend/abi.ts
@@ -1,6 +1,22 @@
 export const abi = [
   {
-    "inputs": [],
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "entropyAddress",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_wagerAmount",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "_usdcAddress",
+        "type": "address"
+      }
+    ],
     "stateMutability": "nonpayable",
     "type": "constructor"
   },
@@ -23,6 +39,44 @@ export const abi = [
       }
     ],
     "name": "_entropyCallback",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes32",
+        "name": "p1hash",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "p2hash",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "serverhash",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "bool",
+        "name": "p1winner",
+        "type": "bool"
+      },
+      {
+        "internalType": "bool",
+        "name": "p2winner",
+        "type": "bool"
+      },
+      {
+        "internalType": "bool",
+        "name": "serverwinner",
+        "type": "bool"
+      }
+    ],
+    "name": "chooseWinner",
     "outputs": [],
     "stateMutability": "nonpayable",
     "type": "function"
@@ -68,7 +122,99 @@ export const abi = [
   },
   {
     "inputs": [],
+    "name": "player1Balance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "player1Ready",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "player2Balance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "player2Ready",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
     "name": "requestRandomNumber",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "usdcAddress",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "wagerAmount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "wagerPlayer1",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "wagerPlayer2",
     "outputs": [],
     "stateMutability": "payable",
     "type": "function"
