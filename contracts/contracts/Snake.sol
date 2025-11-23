84d28f6c6cd85bb64ca7a0cfb39aa07f59ed6c7c
Xavier D'Mello <xaviermdmello@gmail.com>
2025-11-23 00:38:01 -0300

    aa

--- contracts/contracts/Snake.sol
+++ contracts/contracts/Snake.sol
@@ -5,6 +5,7 @@ import { IEntropyV2 } from "@pythnetwork/entropy-sdk-solidity/IEntropyV2.sol";
 contract Snake is IEntropyConsumer {
   IEntropyV2 public entropy;
   bytes32 public number;
+  uint256 public numberAsUint;
  
   constructor() {
     entropy = IEntropyV2(0x41c9e39574F40Ad34c79f1C99B66A45eFB830d4c);
@@ -22,8 +23,9 @@ contract Snake is IEntropyConsumer {
     bytes32 randomNumber
   ) internal override {
     number = randomNumber;
+    numberAsUint = uint256(randomNumber);
   }
- 
+
   // This method is required by the IEntropyConsumer interface.
   // It returns the address of the entropy contract which will call the callback.
   function getEntropy() internal view override returns (address) {
